652b1e3cd3387bf7be9a78e481a0f1d90fed2620
Viktor Gamov <gamussa@gmail.com>
2013-06-27 18:45:49 -0400

    updated font and styles fixed code highlight

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -10,7 +10,7 @@ Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::PostsArchiver.new '/posts', :posts, :archive
   extension Awestruct::Extensions::Tagger.new(:posts, '/index', '/posts/tags', :per_page => 10)
   extension Awestruct::Extensions::TagCloud.new(:tagcloud, '/posts/tags/index.html', :layout => 'post', :title => 'Tags')
-  extension Awestruct::Extensions::Disqus.new
+  extension Awestruct::Extensions::Disqus.new()
 
   # extension Awestruct::Extensions::Indexifier.new
   extension Awestruct::Extensions::Atomizer.new(:posts, '/feed.atom', :feed_title => 'Steeplesoft')
