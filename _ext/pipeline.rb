d815a3cfdf7eb91374c2019363ede88961eae72d
Viktor Gamov <gamussa@gmail.com>
2013-07-09 02:56:46 -0400

    j1 post fixes in other posts

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -7,9 +7,9 @@ require 'posts_archiver'
 Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Posts.new('/posts', :posts)
   extension Awestruct::Extensions::Paginator.new(:posts, '/index', :per_page => 10)
-  extension Awestruct::Extensions::PostsArchiver.new '/posts', :posts, :archive
-  extension Awestruct::Extensions::Tagger.new(:posts, '/index', '/posts/tags', :per_page => 10)
-  extension Awestruct::Extensions::TagCloud.new(:tagcloud, '/posts/tags/index.html', :layout => 'post', :title => 'Tags')
+  extension Awestruct::Extensions::PostsArchiver.new('/posts', :posts, :archive)
+  #extension Awestruct::Extensions::Tagger.new(:posts, '/posts', '/posts/tags', :per_page => 10)
+  #extension Awestruct::Extensions::TagCloud.new(:tagcloud, '/posts/tags/index.html', :layout => 'post', :title => 'Tags')
   extension Awestruct::Extensions::Disqus.new()
 
   # extension Awestruct::Extensions::Indexifier.new
